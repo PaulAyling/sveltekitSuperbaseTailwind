be796f8b044d7c2822d2394f97a31bfcbfc0d1d2
PaulAyling <jimmyyukka@gmail.com>
2022-02-25 16:30:32 +0100

    completed all installations for superbase apart from rollup.js settings

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import {user} from "../sessionStore"
+    import {supabase} from "../supabaseClient"
+    import Auth from "../Auth.svelte"
+    import Profile from "../Profile.svelte"
+
+    user.set(supabase.auth.user())
+
+    supabase.auth.onAuthStateChange((_, session) => {
+        user.set(session.user)
+    })
+</script>
+
+<div class="container" style="padding: 50px 0 100px 0;">
+    {#if $user}
+        <Profile />
+    {:else}
+        <Auth />
+    {/if}
+</div>
