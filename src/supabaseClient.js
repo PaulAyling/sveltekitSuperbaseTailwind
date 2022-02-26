24ecfc92dc8011031baf8e11e6a1f790fba42ee1
PaulAyling <jimmyyukka@gmail.com>
2022-02-26 17:32:25 +0100

    tryinbg to get it to work

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = __api.env.SVELTE_APP_SUPABASE_URL
-const supabaseAnonKey = __api.env.SVELTE_APP_SUPABASE_ANON_KEY
+
+
+const apiUrl =  import.meta.env.VITE_SUPABASE_URL
+const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const supabaseUrl = __api.apiUrl
+const supabaseAnonKey = __api.anonKey
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+console.log('supabase:',supabase)
